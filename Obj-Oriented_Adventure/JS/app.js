0c081494d64c6d44b6a4b60bee3135bbb26d0f7f
Lindsey Weche <lindseyhaishanw22@icloud.com>
2024-05-13 14:15:23 -0400

    Commit 2

--- Obj-Oriented_Adventure/JS/app.js
+++ Obj-Oriented_Adventure/JS/app.js
@@ -5,24 +5,68 @@ const adventurer = {
     companion: {
         name: "Leo",
         type: "Cat",
-        friend:{
+        companion:{
             name : "Frank",
             type : "Flea",
             inventory : ["small hat", "sunglasses"],
         }
     },
-
- roll (mod = 0) {
+    // The roll()
+      roll (mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`);
-     }
+      }
+    
 }
+adventurer.roll();
+
+// Using class to create many adventures 
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+}
+
+// new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+
+class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  }
+
+  class Companion {
+    constructor (name,type){
+       super (name);
+       this.type = type;
+
+    }
+  }
+
     
 
  //To acess any data in the inventory  use .inventory[]
 
-// Now we are console.login each element in the inventory
-for (let i = 0; i<adventurer.inventory.length; i++){
+ // Now we are console.login each element in the inventory
+   for (let i = 0; i<adventurer.inventory.length; i++){
     console.log (adventurer.inventory[i]);
-}
+    }
 
